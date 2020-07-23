b1fba706418795b04e8dd7f4a114ce1aa15447a8
LinJY0424 <f0983344166@gmail.com>
2020-07-23 12:13:54 +0800

    Initial Commit

---
+++ setupIcon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  setupIcon
+//
+//  Created by Duc-Thang Vo on 2020/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
